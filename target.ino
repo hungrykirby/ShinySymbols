898bb633f98672f69c555a7362c41be8a0e0aba5
hungrykirby <hungrykirby@users.noreply.github.com>
2022-01-09 13:51:31 +0900

    リファクタ：無駄なcase文の短縮とif文の関数への移行 (#10)
    * 無駄なcase文の短縮とif文の関数への移行
    
    * 対象ポケモンを指定する関数を外部ファイル化
    
    * 改行した

---
+++ target.ino
@@ -0,0 +1,12 @@
+/*
+ *
+ * 捕まえたいポケモンをの定数を指定する
+ * Gitの差分がポケモンを変えるたびにShinySymbols.inoで出て来るのを避けるため
+ * 本当は設定ファイルみたいなのを持ったほうがいいのかもしれない
+ *
+ */
+
+// return: int
+int setTarget(){
+    return STOUTLAND;
+}
